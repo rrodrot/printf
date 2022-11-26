9039020e16a13d68335ac2576413a272075247e8
Richard <rrodrot@gmail.com>
2022-11-26 04:58:21 -0800

    Functions1.c

--- functions1.c
+++ functions1.c
@@ -129,6 +129,7 @@ int flags, int width, int precision, int size)
 int print_hexa(va_list types, char map_to[], char buffer[],
 int flags, char flag_ch, int width, int precision, int size)
 {
+
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
 	unsigned long int init_num = num;
